5172794c13f81aa5f639ad98324cfdc44b0fabdc
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-03-25 12:16:41 -0400

    Move manager creations to top level
    Since main.js is declared last, there are no worries about instantiating an object before its source code has been loaded.
    
    Global variables aren’t the greatest thing in the world, but at least it’ll be easier to point them out now.

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ var persistence = new Persistence();
 var showHitboxes = persistence.getBoolean('show_hitboxes', false);
 var forceDefaultAssets = persistence.getBoolean('force_default_assets', false);
 
-var game;
+var game = new Game();
 var context2d;
-var keyboard;
-var audioManager;
-var imageManager;
+var keyboard = new Keyboard();
+var audioManager = new AudioManager();
+var imageManager = new ImageManager();
 var stringsManager = new StringsManager();
 var configManager = new ConfigManager();
 var configAudioManager = new ConfigAudioManager();
@@ -23,8 +23,6 @@ window.addEventListener('load', function() {
  * Sets up the HTML page and instantiates the game object.
  */
 function createGame() {
-  game = new Game();
-
   var rootDiv = document.getElementById('game');
   var canvas = document.createElement('canvas');
   canvas.width = game.width;
@@ -32,12 +30,8 @@ function createGame() {
   context2d = canvas.getContext('2d');
   rootDiv.appendChild(canvas);
 
-  keyboard = new Keyboard();
   document.addEventListener('keydown', keydown);
   document.addEventListener('keyup', keyup);
-
-  audioManager = new AudioManager();
-  imageManager = new ImageManager();
 
   setInterval(update, 1000 / game.fps);
 }
